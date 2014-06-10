28348b47eb671ed5fe420e758c9288f2065fcee5
David Donnelly <daviddonnelly@Davids-MacBook-Pro.local>
2014-06-10 17:59:20 +0100

    added a trest class

---
+++ Git Test/DDTest.h
@@ -0,0 +1,13 @@
+//
+//  DDTest.h
+//  Git Test
+//
+//  Created by David Donnelly on 10/06/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDTest : NSObject
+
+@end
---
+++ Git Test/DDTest.m
@@ -0,0 +1,13 @@
+//
+//  DDTest.m
+//  Git Test
+//
+//  Created by David Donnelly on 10/06/2014.
+//
+//
+
+#import "DDTest.h"
+
+@implementation DDTest
+
+@end
